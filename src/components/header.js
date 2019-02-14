2809c4c3098a3af448fece29d5db8b507abcff10
Eric Hodges <14981329+erichodges@users.noreply.github.com>
2019-02-14 14:38:46 -0800

    updated header

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,56 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import React from "react";
+import { Link } from "gatsby";
+import PropTypes from "prop-types";
+import gatsbyLogo from "../images/gatsby-icon.png";
 
 const Header = ({ siteTitle }) => (
   <header
     style={{
       background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
+      marginBottom: `1.45rem`
     }}
   >
     <div
       style={{
         margin: `0 auto`,
         maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        padding: `1.45rem 1.0875rem`
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
+      {/* Title / Logo */}
+      <span style={{ display: "flex", alignItems: "center" }}>
+        <img
+          src={gatsbyLogo}
+          alt="Gatsby Logo"
           style={{
-            color: `white`,
-            textDecoration: `none`,
+            border: "3px solid orange",
+            borderRadius: "50%",
+            margin: "0 5px",
+            width: "50px"
           }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+        />
+        <h1 style={{ margin: 0 }}>
+          <Link
+            to="/"
+            style={{
+              color: `white`,
+              textDecoration: `none`
+            }}
+          >
+            {siteTitle}
+          </Link>
+        </h1>
+      </span>
     </div>
   </header>
-)
+);
 
 Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
+  siteTitle: PropTypes.string
+};
 
 Header.defaultProps = {
-  siteTitle: ``,
-}
+  siteTitle: ``
+};
 
-export default Header
+export default Header;
